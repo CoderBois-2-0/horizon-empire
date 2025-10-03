c5f403e08f93507dc0cf68ac0db2f16735500a8b
doro-hd <doro-hd@protonmail.com>
2025-10-03 12:27:02 +0200

    Add reference between map and region

--- src/db/map/schema.ts
+++ src/db/map/schema.ts
@@ -1,14 +1,23 @@
+import { regionsTable } from "$db/region/schema.js";
+import { relations } from "drizzle-orm";
 import { pgTable, pgEnum, varchar } from "drizzle-orm/pg-core";
 
 const mapType = pgEnum("map_type", ["forrest", "arctic", "desert", "bay"]);
+const mapSize = pgEnum("map_size", ["small", "medium", "large"]);
 
 const mapTable = pgTable("map", {
 	id: varchar("id", { length: 36 }).primaryKey(),
-	name: varchar("name", { length: 32 }).notNull(),
-	startingRegionID: varchar("starting_region_id", { length: 36 }).notNull()
+	type: mapType("type").notNull(),
+	size: mapSize("map_size").notNull(),
+	startingRegionID: varchar("starting_region_id", { length: 36 }),
 });
 
-export {
-	mapTable,
-	mapType
-};
+const mapRelation = relations(mapTable, ({ one }) => ({
+	startingRegion: one(regionsTable, {
+		fields: [mapTable.startingRegionID],
+		references: [regionsTable.id],
+
+	}),
+}));
+
+export { mapTable, mapType, mapSize, mapRelation };
--- src/db/region/schema.ts
+++ src/db/region/schema.ts
@@ -1,11 +1,24 @@
-import { pgTable, varchar } from "drizzle-orm/pg-core";
+import { mapTable } from "$db/map/schema.js";
+import { relations } from "drizzle-orm";
+import { pgTable, foreignKey, varchar } from "drizzle-orm/pg-core";
 
-const regionsTable = pgTable("region", {
-	id: varchar("id", { length: 36 }).primaryKey(),
-	name: varchar("name", { length: 32 }).notNull(),
-	mapID: varchar("map_id", { length: 36 }).notNull()
-});
+const regionsTable = pgTable(
+	"region",
+	{
+		id: varchar("id", { length: 36 }).primaryKey(),
+		name: varchar("name", { length: 32 }).notNull(),
+		mapID: varchar("map_id", { length: 36 }).notNull(),
+	},
+	(table) => [
+		foreignKey({ columns: [table.mapID], foreignColumns: [mapTable.id] }),
+	],
+);
 
-export {
-	regionsTable
-}
+const regionRelation = relations(regionsTable, ({ one }) => ({
+	map: one(mapTable, {
+		fields: [regionsTable.mapID],
+		references: [mapTable.id]
+	})
+}))
+
+export { regionsTable, regionRelation };
