254245fadad1990fd8cdb69cee7e6f9c4cf53fdc
Lise Freja Jensen Petculescu <124042840+LisePetculescu@users.noreply.github.com>
2025-10-05 22:45:45 +0200

    Cb 18 create tile type enum (#9)
    * esbuild as build dependency
    
    * added tiletype enum schema
    
    * tile type enum migration file

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,11 +1,12 @@
 import { drizzle } from "drizzle-orm/neon-http";
-import * as mapShcema from "./map/schema.js";
+import * as mapSchema from "./map/schema.js";
 import * as regionSchema from "./region/schema.js";
 import * as userSchema from "./user/schema.js";
+import * as tileTypeSchema from "./tileType/schema.js";
 
 function connectDB(dbUrl: string) {
   return drizzle(dbUrl, {
-    schema: { ...userSchema, ...regionSchema, ...mapShcema },
+    schema: { ...userSchema, ...regionSchema, ...mapSchema, ...tileTypeSchema },
   });
 }
 
