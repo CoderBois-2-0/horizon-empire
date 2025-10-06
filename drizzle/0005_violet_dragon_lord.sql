ba76626f578349d13597cc18e7e761fb613ad2a8
lasse rugaard rasmussen <lass531n@stud.kea.dk>
2025-10-06 19:16:37 +0200

    generated new migration files for resourceInventory

--- drizzle/0005_violet_dragon_lord.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE "resource_inventories" (
-	"id" varchar(36) PRIMARY KEY NOT NULL,
-	"city_id" varchar(36) NOT NULL
-);
---> statement-breakpoint
-ALTER TABLE "resource_inventories" ADD CONSTRAINT "resource_inventories_city_id_cities_id_fk" FOREIGN KEY ("city_id") REFERENCES "public"."cities"("id") ON DELETE cascade ON UPDATE no action;
