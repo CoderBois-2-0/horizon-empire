104cb53358a53bc5cdfe3c8cc02aac1d4690f42a
Lise Freja Jensen Petculescu <124042840+LisePetculescu@users.noreply.github.com>
2025-10-06 17:06:39 +0200

    Cb 20 create building table (#11)
    * W! not done, needs tile type enum, migration file and to be added to index
    
    * building table schema added including building type enum
    
    * building table schema migration files
    
    * formatting fixes
    
    * formatting fix
    
    * formatting fix #3
    
    * Add schema changes to new migration file
    
    * Format
    
    * import update for  building in index
    
    * removed tile schema from index, as it is not yet present
    
    ---------
    
    Co-authored-by: doro-hd <doro-hd@protonmail.com>

--- src/db/index.ts
+++ src/db/index.ts
@@ -3,7 +3,7 @@ import * as mapSchema from "./map/schema.js";
 import * as regionSchema from "./region/schema.js";
 import * as userSchema from "./user/schema.js";
 import * as tileTypeSchema from "./tileType/schema.js";
-import * as tileSchema from "./tile/schema.js";
+import * as buildingTable from "./building/schema.js";
 
 function connectDB(dbUrl: string) {
   return drizzle(dbUrl, {
@@ -12,7 +12,7 @@ function connectDB(dbUrl: string) {
       ...regionSchema,
       ...mapSchema,
       ...tileTypeSchema,
-      ...tileSchema,
+      ...buildingTable,
     },
   });
 }
