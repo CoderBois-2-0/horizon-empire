cceb614c51f0de3544b1bc816894e90afe36ed7c
lasse rugaard rasmussen <lass531n@stud.kea.dk>
2025-10-06 15:36:51 +0200

    ran lint --fix

--- src/db/city/schema.ts
+++ src/db/city/schema.ts
@@ -3,16 +3,16 @@ import { mapTable } from "../map/schema.js";
 import { pgTable, varchar } from "drizzle-orm/pg-core";
 
 const cityTable = pgTable("cities", {
-    id: varchar("id", { length: 36 }).primaryKey(),
-    name: varchar("name", { length: 64 }).notNull(),
+  id: varchar("id", { length: 36 }).primaryKey(),
+  name: varchar("name", { length: 64 }).notNull(),
 
-    user_id: varchar("user_id", { length: 36 })
-        .notNull()
-        .references(() => userTable.id, { onDelete: "cascade" }),
+  user_id: varchar("user_id", { length: 36 })
+    .notNull()
+    .references(() => userTable.id, { onDelete: "cascade" }),
 
-    map_id: varchar("map_id", { length: 36 })
-        .notNull()
-        .references(() => mapTable.id, { onDelete: "cascade" }),
-    });
+  map_id: varchar("map_id", { length: 36 })
+    .notNull()
+    .references(() => mapTable.id, { onDelete: "cascade" }),
+});
 
 export { cityTable };
