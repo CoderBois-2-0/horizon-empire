ace77b82881f6f154cdcf5d9df897c1de7f81ac7
lasse rugaard rasmussen <lass531n@stud.kea.dk>
2025-10-06 15:26:06 +0200

    created city table

---
+++ src/db/city/schema.ts
@@ -0,0 +1,18 @@
+import { userTable } from "../user/schema.js";
+import { mapTable } from "../map/schema.js";
+import { pgTable, varchar } from "drizzle-orm/pg-core";
+
+const cityTable = pgTable("cities", {
+    id: varchar("id", { length: 36 }).primaryKey(),
+    name: varchar("name", { length: 64 }).notNull(),
+
+    user_id: varchar("user_id", { length: 36 })
+        .notNull()
+        .references(() => userTable.id, { onDelete: "cascade" }),
+
+    map_id: varchar("map_id", { length: 36 })
+        .notNull()
+        .references(() => mapTable.id, { onDelete: "cascade" }),
+    });
+
+export { cityTable };
