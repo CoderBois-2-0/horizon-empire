140f42b4e63680f480e75f89b1f003e3d3ea40f7
Lasse-R <91476388+Lasse-R@users.noreply.github.com>
2025-10-06 18:09:19 +0200

    created city table (#12)
    * created city table
    
    * ran lint --fix
    
    * havde glemt at tilføje citySchema til db/index. Det er fixed
    
    * fixed user and map imports to use / instead of ../ and generated migration files
    
    * Add migration files
    
    * Format
    
    * Remove incorrect migration file
    
    * fixed variable names to follow naming standards
    
    * fixed city name varchar to the correct length
    
    * value in cityTable name corrected from 36 to 32 and new migration files generated
    
    ---------
    
    Co-authored-by: doro-hd <doro-hd@protonmail.com>

---
+++ src/db/city/schema.ts
@@ -0,0 +1,18 @@
+import { userTable } from "$db/user/schema.js";
+import { mapTable } from "$db/map/schema.js";
+import { pgTable, varchar } from "drizzle-orm/pg-core";
+
+const cityTable = pgTable("cities", {
+  id: varchar("id", { length: 36 }).primaryKey(),
+  name: varchar("name", { length: 32 }).notNull(),
+
+  userID: varchar("user_id", { length: 36 })
+    .notNull()
+    .references(() => userTable.id, { onDelete: "cascade" }),
+
+  mapID: varchar("map_id", { length: 36 })
+    .notNull()
+    .references(() => mapTable.id, { onDelete: "cascade" }),
+});
+
+export { cityTable };
--- src/db/index.ts
+++ src/db/index.ts
@@ -3,6 +3,8 @@ import * as mapSchema from "./map/schema.js";
 import * as regionSchema from "./region/schema.js";
 import * as userSchema from "./user/schema.js";
 import * as tileTypeSchema from "./tileType/schema.js";
+import * as tileSchema from "./tile/schema.js";
+import * as citySchema from "./city/schema.js";
 import * as buildingTable from "./building/schema.js";
 
 function connectDB(dbUrl: string) {
@@ -12,6 +14,8 @@ function connectDB(dbUrl: string) {
       ...regionSchema,
       ...mapSchema,
       ...tileTypeSchema,
+      ...tileSchema,
+      ...citySchema,
       ...buildingTable,
     },
   });
