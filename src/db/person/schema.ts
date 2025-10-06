523edc56778dce6867541efd0370a45c916ea871
Lise Freja Jensen Petculescu <124042840+LisePetculescu@users.noreply.github.com>
2025-10-06 19:44:43 +0200

    person schema update

--- src/db/person/schema.ts
+++ src/db/person/schema.ts
@@ -1,13 +1,26 @@
+import { placedBuildingTable } from "$db/placedBuilding/schema.js";
+import { foreignKey } from "drizzle-orm/pg-core";
 import { pgTable, pgEnum, varchar } from "drizzle-orm/pg-core";
 
 const ageCategory = pgEnum("age_category", ["newborn", "child", "teen", "young adult", "adult", "senior"]);
 const incomeGroup = pgEnum("income_group", ["low", "medium", "high"]);
 
-// id VARCHAR(36), age_category age_category, income_group income_group, housing_id VARCHAR(36)
+const personTable = pgTable(
+  "persons",
+  {
+    id: varchar("id", { length: 36 }).primaryKey(),
+    ageCategory: ageCategory("age_category").notNull(),
+    incomeGroup: incomeGroup("income_group").notNull(),
+    housingID: varchar("housing_id", { length: 36 })
+      .notNull()
+      .references(() => placedBuildingTable.id, { onDelete: "cascade" }),
+  },
+  (table) => [
+    foreignKey({
+      columns: [table.housingID],
+      foreignColumns: [placedBuildingTable.id],
+    }),
+  ]
+);
 
-const personTable = pgTable("persons", {
-  id: varchar("id", { length: 36 }).primaryKey(),
-  ageCategory: ageCategory("age_category").notNull(),
-  incomeGroup: incomeGroup("income_group").notNull(),
-
-});
+export { personTable, ageCategory, incomeGroup };
