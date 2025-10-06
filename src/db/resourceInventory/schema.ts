dc4cf0a970850ca6c8468a7bf309cdab4ea6bb80
lasse rugaard rasmussen <lass531n@stud.kea.dk>
2025-10-06 18:31:12 +0200

    lint fix

--- src/db/resourceInventory/schema.ts
+++ src/db/resourceInventory/schema.ts
@@ -3,7 +3,7 @@ import { pgTable, varchar } from "drizzle-orm/pg-core";
 
 const resourceInventoryTable = pgTable("resource_inventories", {
   id: varchar("id", { length: 36 }).primaryKey(),
-  
+
   cityID: varchar("city_id", { length: 36 })
     .notNull()
     .references(() => cityTable.id, { onDelete: "cascade" }),
