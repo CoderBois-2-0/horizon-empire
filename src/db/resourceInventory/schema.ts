2359520b94d6f291e4a55e5e920a74c1de4623d0
Lasse-R <91476388+Lasse-R@users.noreply.github.com>
2025-10-06 19:21:47 +0200

    Cb 8 create resource inventory table (#13)
    * resource_inventory table added
    
    * added resource inventory to db/index
    
    * generated migration files for resource inventory
    
    * lint fix
    
    * added foreignKey to resourceInventory
    
    * generated new migration files for resourceInventory
    
    * linted

---
+++ src/db/resourceInventory/schema.ts
@@ -0,0 +1,20 @@
+import { cityTable } from "../city/schema.js";
+import { foreignKey, pgTable, varchar } from "drizzle-orm/pg-core";
+
+const resourceInventoryTable = pgTable(
+  "resource_inventories",
+  {
+    id: varchar("id", { length: 36 }).primaryKey(),
+    cityID: varchar("city_id", { length: 36 })
+      .notNull()
+      .references(() => cityTable.id, { onDelete: "cascade" }),
+  },
+  (table) => [
+    foreignKey({
+      columns: [table.cityID],
+      foreignColumns: [cityTable.id],
+    }),
+  ],
+);
+
+export { resourceInventoryTable };
