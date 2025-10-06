d2bbafbd15bf744a555f00574f3be51d2074923f
lasse rugaard rasmussen <lass531n@stud.kea.dk>
2025-10-06 18:25:34 +0200

    resource_inventory table added

---
+++ src/db/resourceInventory/schema.ts
@@ -0,0 +1,12 @@
+import { cityTable } from "../city/schema.js";
+import { pgTable, varchar } from "drizzle-orm/pg-core";
+
+const resourceInventoryTable = pgTable("resource_inventories", {
+  id: varchar("id", { length: 36 }).primaryKey(),
+  
+  cityID: varchar("city_id", { length: 36 })
+    .notNull()
+    .references(() => cityTable.id, { onDelete: "cascade" }),
+});
+
+export { resourceInventoryTable };
