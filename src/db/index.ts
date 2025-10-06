84aa0e77c1b6b80ad0332fd3c21336c562509d5d
Lise Freja Jensen Petculescu <124042840+LisePetculescu@users.noreply.github.com>
2025-10-06 08:26:58 +0200

    Cb 17 create tile table (#10)
    * W! waiting for tile type schema to be merged into main... tile table schema added
    
    * formatting fixes
    
    * tile table migration files

--- src/db/index.ts
+++ src/db/index.ts
@@ -3,10 +3,17 @@ import * as mapSchema from "./map/schema.js";
 import * as regionSchema from "./region/schema.js";
 import * as userSchema from "./user/schema.js";
 import * as tileTypeSchema from "./tileType/schema.js";
+import * as tileSchema from "./tile/schema.js";
 
 function connectDB(dbUrl: string) {
   return drizzle(dbUrl, {
-    schema: { ...userSchema, ...regionSchema, ...mapSchema, ...tileTypeSchema },
+    schema: {
+      ...userSchema,
+      ...regionSchema,
+      ...mapSchema,
+      ...tileTypeSchema,
+      ...tileSchema,
+    },
   });
 }
 
---
+++ src/db/tile/schema.ts
@@ -0,0 +1,20 @@
+import { pgTable, varchar, foreignKey } from "drizzle-orm/pg-core";
+import { regionsTable } from "$db/region/schema.js";
+import { tileType } from "$db/tileType/schema.js";
+
+const tileTable = pgTable(
+  "tile",
+  {
+    id: varchar("id", { length: 36 }).primaryKey(),
+    type: tileType("type").notNull(),
+    regionID: varchar("region_id", { length: 36 }),
+  },
+  (table) => [
+    foreignKey({
+      columns: [table.regionID],
+      foreignColumns: [regionsTable.id],
+    }),
+  ],
+);
+
+export { tileTable };
