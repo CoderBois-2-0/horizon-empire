a0d1d61b7629dd845735ce8a7d830a5a2a1e30e4
lasse rugaard rasmussen <lass531n@stud.kea.dk>
2025-10-06 19:53:59 +0200

    resource junction added

---
+++ src/db/resourceJunction/schema.ts
@@ -0,0 +1,29 @@
+import { foreignKey, pgTable, varchar, integer } from "drizzle-orm/pg-core";
+import { resourceTable } from "$db/resource/schema.js";
+import { resourceInventoryTable } from "$db/resourceInventory/schema.js";
+
+const resourceJunctionTable = pgTable(
+  "resource_junctions",
+  {
+    id: varchar("id", { length: 36 }).primaryKey(),
+    resourceID: varchar("resource_id", { length: 36 })
+      .notNull()
+      .references(() => resourceTable.id, { onDelete: "cascade" }),
+    inventoryID: varchar("inventory_id", { length: 36 })
+      .notNull()
+      .references(() => resourceInventoryTable.id, { onDelete: "cascade" }),
+    quantity: integer("quantity").notNull().default(0),
+  },
+  (table) => [
+    foreignKey({
+      columns: [table.resourceID],
+      foreignColumns: [resourceTable.id],
+    }),
+    foreignKey({
+      columns: [table.inventoryID],
+      foreignColumns: [resourceInventoryTable.id],
+    }),
+  ],
+);
+
+export { resourceJunctionTable };
