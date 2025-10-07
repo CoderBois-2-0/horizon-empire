5ed78ed20ecc4ffca522aceb999d34a23caf789c
doro-hd <doro-hd@protonmail.com>
2025-10-07 08:45:03 +0200

    Format

--- src/db/region/schema.ts
+++ src/db/region/schema.ts
@@ -3,27 +3,31 @@ import { getTableColumns, relations, sql } from "drizzle-orm";
 import { pgTable, foreignKey, varchar, pgView } from "drizzle-orm/pg-core";
 
 const regionsTable = pgTable(
-	"region",
-	{
-		id: varchar("id", { length: 36 }).primaryKey(),
-		name: varchar("name", { length: 32 }).notNull(),
-		mapID: varchar("map_id", { length: 36 }).notNull(),
-	},
-	(table) => [
-		foreignKey({ columns: [table.mapID], foreignColumns: [mapTable.id] }),
-	],
+  "region",
+  {
+    id: varchar("id", { length: 36 }).primaryKey(),
+    name: varchar("name", { length: 32 }).notNull(),
+    mapID: varchar("map_id", { length: 36 }).notNull(),
+  },
+  (table) => [
+    foreignKey({ columns: [table.mapID], foreignColumns: [mapTable.id] }),
+  ],
 );
 
 const regionRelation = relations(regionsTable, ({ one }) => ({
-	map: one(mapTable, {
-		fields: [regionsTable.mapID],
-		references: [mapTable.id],
-	}),
+  map: one(mapTable, {
+    fields: [regionsTable.mapID],
+    references: [mapTable.id],
+  }),
 }));
 
-const regionWithTotalTiles = pgView("region_with_total_tiles").as(qb => qb.select({
-	...getTableColumns(regionsTable),
-	totalTiles: sql`total_region_tiles(${regionsTable.id})`.as("total_tiles")
-}).from(regionsTable))
+const regionWithTotalTiles = pgView("region_with_total_tiles").as((qb) =>
+  qb
+    .select({
+      ...getTableColumns(regionsTable),
+      totalTiles: sql`total_region_tiles(${regionsTable.id})`.as("total_tiles"),
+    })
+    .from(regionsTable),
+);
 
 export { regionsTable, regionRelation, regionWithTotalTiles };
