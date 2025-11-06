41eb886debe872637f707a3ed979f54ca46509bc
lasse rugaard rasmussen <lass531n@stud.kea.dk>
2025-11-07 00:34:49 +0100

    cb-61-create-building_resource_cost-table

---
+++ src/db/buildingResourceCost/schema.ts
@@ -0,0 +1,30 @@
+import {
+  integer,
+  pgTable,
+  varchar,
+  primaryKey,
+  foreignKey,
+} from "drizzle-orm/pg-core";
+import { buildingTable } from "$db/building/schema.js";
+import { resourceTable } from "$db/resource/schema.js";
+
+const buildingResourceCostTable = pgTable(
+  "building_resource_cost",
+  {
+    buildingID: varchar("building_id", { length: 36 }).notNull(),
+    resourceID: varchar("resource_id", { length: 36 }).notNull(),
+    amount: integer("amount").notNull(),
+  },
+  (table) => [
+    primaryKey({ columns: [table.buildingID, table.resourceID] }),
+    foreignKey({
+      columns: [table.buildingID],
+      foreignColumns: [buildingTable.id],
+    }).onDelete("cascade"),
+    foreignKey({
+      columns: [table.resourceID],
+      foreignColumns: [resourceTable.id],
+    }).onDelete("cascade"),
+  ],
+);
+export { buildingResourceCostTable };
