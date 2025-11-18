593334eb35c4241d77c579e3ceb685bd11fc8251
lasse rugaard rasmussen <lass531n@stud.kea.dk>
2025-11-18 08:24:10 +0100

    pull before push

--- worker-configuration.d.ts
+++ worker-configuration.d.ts
@@ -7608,7 +7608,7 @@ type AIGatewayHeaders = {
   [key: string]: string | number | boolean | object;
 };
 type AIGatewayUniversalRequest = {
-  provider: AIGatewayProviders | string; // eslint-disable-line
+  provider: AIGatewayProviders | string;  
   endpoint: string;
   headers: Partial<AIGatewayHeaders>;
   query: unknown;
@@ -7625,7 +7625,7 @@ declare abstract class AiGateway {
       extraHeaders?: object;
     },
   ): Promise<Response>;
-  getUrl(provider?: AIGatewayProviders | string): Promise<string>; // eslint-disable-line
+  getUrl(provider?: AIGatewayProviders | string): Promise<string>;  
 }
 interface AutoRAGInternalError extends Error {}
 interface AutoRAGNotFoundError extends Error {}
