9e1d6d97278611e777e865216b59119d811e5341
doro-hd <doro-hd@protonmail.com>
2025-11-18 08:56:33 +0100

    Format

--- src/db/map/schema.ts
+++ src/db/map/schema.ts
@@ -4,9 +4,9 @@ const mapType = pgEnum("map_type", ["forrest", "arctic", "desert", "bay"]);
 const mapSize = pgEnum("map_size", ["small", "medium", "large"]);
 
 const mapTable = pgTable("maps", {
-	id: varchar("id", { length: 36 }).primaryKey(),
-	type: mapType("type").notNull(),
-	size: mapSize("map_size").notNull(),
+  id: varchar("id", { length: 36 }).primaryKey(),
+  type: mapType("type").notNull(),
+  size: mapSize("map_size").notNull(),
 });
 
 export { mapTable, mapType, mapSize };
