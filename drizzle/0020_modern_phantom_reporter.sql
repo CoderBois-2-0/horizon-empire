5bd1e930fbec80d24ca732c5eb15f68d9ef5e19d
doro-hd <doro-hd@protonmail.com>
2025-11-22 13:05:10 +0100

    Drop outdated migration

--- drizzle/0020_modern_phantom_reporter.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE "regions" DROP CONSTRAINT "regions_map_id_maps_id_fk";
---> statement-breakpoint
-ALTER TABLE "tiles" DROP CONSTRAINT "tiles_region_id_regions_id_fk";
---> statement-breakpoint
-ALTER TABLE "regions" ADD CONSTRAINT "regions_map_id_maps_id_fk" FOREIGN KEY ("map_id") REFERENCES "public"."maps"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
-ALTER TABLE "tiles" ADD CONSTRAINT "tiles_region_id_regions_id_fk" FOREIGN KEY ("region_id") REFERENCES "public"."regions"("id") ON DELETE cascade ON UPDATE no action;
