a7056e63e756f93955632a91855399c0b7fd6806
Lise Freja Jensen Petculescu <124042840+LisePetculescu@users.noreply.github.com>
2025-11-24 12:12:49 +0100

    removing tileType enum from placedBuilding

--- src/db/placedBuilding/schema.ts
+++ src/db/placedBuilding/schema.ts
@@ -1,5 +1,4 @@
 import { pgTable, varchar, foreignKey } from "drizzle-orm/pg-core";
-import { tileType } from "$db/tileType/schema.js";
 import { buildingTable } from "$db/building/schema.js";
 import { cityTable } from "$db/city/schema.js";
 
@@ -9,7 +8,6 @@ const placedBuildingTable = pgTable(
     id: varchar("id", { length: 36 }).primaryKey(),
     buildingID: varchar("building_id", { length: 36 }).notNull(),
     cityID: varchar("city_id", { length: 36 }).notNull(),
-    buildOnTiles: tileType("build_on_tiles").array().notNull(),
   },
   (table) => [
     foreignKey({
