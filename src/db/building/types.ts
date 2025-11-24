605413b0ba0eed8d14f3697d9524a7c19755116b
lasse rugaard rasmussen <lass531n@stud.kea.dk>
2025-11-24 02:12:18 +0100

    removed validation from  getAll on buildings

--- src/db/building/types.ts
+++ src/db/building/types.ts
@@ -3,7 +3,6 @@ import { buildingTable } from "./schema";
 type IBuilding = typeof buildingTable.$inferSelect;
 
 type IBuildingQuery = Partial<{
-  id: string;
 }>;
 
 export { IBuilding, IBuildingQuery };
--- src/routers/building/index.ts
+++ src/routers/building/index.ts
@@ -1,13 +1,11 @@
 import { createRouter, TContext, IEnv } from "$routers/index";
 import { BuildingHandler } from "$db/building/handler";
 import { IBuilding } from "$db/building/types";
-import { buildingQueryValidator } from "./validation";
 
 const buildingRouter = createRouter();
 
 buildingRouter.get(
   "/buildings",
-  buildingQueryValidator,
   async (c: TContext<IEnv>) => {
     const buildingHandler = new BuildingHandler(c.env.DB_URL);
 
