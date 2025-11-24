b1eec7ded3de70b7011dbc4263992530b0e1bf72
lasse rugaard rasmussen <lass531n@stud.kea.dk>
2025-11-24 01:49:43 +0100

    changed regionQueryValidator to only validate mapID

--- src/db/region/types.ts
+++ src/db/region/types.ts
@@ -3,7 +3,5 @@ import { regionsTable } from "./schema";
 export type IRegion = typeof regionsTable.$inferSelect;
 
 export interface IRegionQuery {
-  name?: string;
   mapID?: string;
-  isUnlocked?: boolean;
 }
