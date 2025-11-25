9083509565e461d1beb3b0ee188bdd6d41dab182
Lasse-R <91476388+Lasse-R@users.noreply.github.com>
2025-11-25 15:20:16 +0100

    Cb 77 add get all not placed buildings logic (#41)
    * cb-77-add-get-all-not-placed-buildings-logic
    
    * lint
    
    * removed validation from  getAll on buildings
    
    * merged with main and linted

---
+++ src/routers/building/index.ts
@@ -0,0 +1,15 @@
+import { createRouter, TContext, IEnv } from "$routers/index";
+import { BuildingHandler } from "$db/building/handler";
+import { IBuilding } from "$db/building/types";
+
+const buildingRouter = createRouter();
+
+buildingRouter.get("/buildings", async (c: TContext<IEnv>) => {
+  const buildingHandler = new BuildingHandler(c.env.DB_URL);
+
+  const buildings: IBuilding[] = await buildingHandler.getAll();
+
+  return c.json({ buildings });
+});
+
+export { buildingRouter };
