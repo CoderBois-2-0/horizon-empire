84db86ff7ab72106099f6803fd5d3183bef7b804
Lise Freja Jensen Petculescu <124042840+LisePetculescu@users.noreply.github.com>
2026-01-03 17:47:41 +0100

    should be changed back to deployed db

--- migration/postgresql/db.ts
+++ migration/postgresql/db.ts
@@ -3,7 +3,8 @@ import { connectDB } from "../../src/db/index.js";
 export type TDB = ReturnType<typeof connectDB>;
 
 export function connectToDB() {
-  const dbUrl = process.env.DATABASE_URL;
+  const dbUrl = process.env.DB_LOCAL;
+  // const dbUrl = process.env.DATABASE_URL;
   if (!dbUrl) throw new Error("DB environment variable is not set");
 
   return connectDB(dbUrl);
